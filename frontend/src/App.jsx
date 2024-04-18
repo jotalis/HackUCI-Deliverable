e16ad673aaeb08db3aec9d4dcfee757ef7e9125e
Jay Wu <jayqw@uci.edu>
2024-04-18 02:14:21 -0700

    Move code to component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,7 @@ import QuoteTable from "./components/QuoteTable/QuoteTable";
 
 function App() {
 	
-	const [ageFilter, setAgeFilter] = useState("all");
 	const [quotes, setQuotes] = useState([]);
-
-	const ageFilterChange = (event) => {
-		console.log(event.target.value);
-		setAgeFilter(event.target.value);
-	}
-
-	useEffect(() => {
-		fetch(`/api/retrieve?max_age=${ageFilter}`)
-		.then(response => response.json())
-		.then(data => setQuotes(data))
-	}, [ageFilter]);
 
 	return (
 		<div className="App">
@@ -32,14 +20,7 @@ function App() {
 				<input type="text" name="message" id="input-message" required />
 				<button type="submit">Submit</button>
 			</form>
-			<select id="input-age" selected="all" name="age filter" value={ageFilter} onChange={ageFilterChange}>
-				<option value="all">All</option>
-				<option value="day">1 day</option>
-				<option value="week">1 week</option>
-				<option value="month">1 month</option>
-				<option value="year">1 year</option>
-			</select>
-			<QuoteTable quotes={quotes} />
+			<QuoteTable quotes={quotes} setQuotes={setQuotes} />
 		</div>
 	);
 }
--- frontend/src/components/QuoteTable/QuoteTable.jsx
+++ frontend/src/components/QuoteTable/QuoteTable.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './QuoteTable.css';
 
-function QuoteTable({quotes}) {
+function QuoteTable({quotes, setQuotes}) {
+
+    // handle age filter states
+    const [ageFilter, setAgeFilter] = useState("all");
+
+	const ageFilterChange = (event) => {
+		console.log(event.target.value);
+		setAgeFilter(event.target.value);
+	}
+
+	useEffect(() => {
+		fetch(`/api/retrieve?max_age=${ageFilter}`)
+		.then(response => response.json())
+		.then(data => setQuotes(data))
+	}, [ageFilter]);
 
     // format dates from database before displaying
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: '2-digit', day: '2-digit'};
         return new Date(dateString).toLocaleDateString(undefined, options);
     };
+    
     return (
         <div className="quote-table-container">
-            <h1 className="quote-table-title">Recent Quotes</h1>    
+            <h1 className="quote-table-title">Recent Quotes</h1>   
+            <select id="input-age" className="quote-table-filter" selected="all" name="age filter" value={ageFilter} onChange={ageFilterChange}>
+                <option value="all">All</option>
+                <option value="day">1 day</option>
+                <option value="week">1 week</option>
+                <option value="month">1 month</option>
+                <option value="year">1 year</option>
+            </select> 
             <table className="quote-table">
                 <thead>
                     <tr>
